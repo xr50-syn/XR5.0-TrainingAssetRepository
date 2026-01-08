6c81593429c0ca789321805d4a7d83fe3562b046
emm-n-m <emm.n.black@gmail.com>
2026-01-08 14:05:52 +0200

    Added comprehensive test suite

--- tests/functional/jest.config.js
+++ tests/functional/jest.config.js
@@ -7,10 +7,6 @@ module.exports = {
   detectOpenHandles: true,
   globalSetup: './setup.js',
   globalTeardown: './teardown.js',
-  reporters: [
-    'default',
-    ['jest-summary-reporter', { failuresOnly: false }]
-  ],
   // Run tests in order (not parallel) to ensure proper sequencing
   maxWorkers: 1
 };
